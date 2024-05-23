5833c6bf5c2a5b59f189f5288964a755e0bb3eb6
Wangyouming <166718326+wangyoumean@users.noreply.github.com>
2024-05-23 10:37:00 +0800

    重新提交
    由于文件夹命名混乱，上次提交了错误的文件......

--- ball.cpp
+++ ball.cpp
@@ -1,101 +1,92 @@
 #include "ball.h"
 #include "ui_ball.h"
 
-Ball::Ball(QWidget *parent) : QWidget(parent), ui(new Ui::Ball),border(nullptr)//初始化
-{
-    ui->setupUi(this);
-    // 初始化小球的其他属性
-    ballX = 0;
-    ballY = 0;
-    r = 10; // 默认半径
-    dx = 0;
-    dy = 0;
+Ball::Ball(float ballX, float ballY, float dx, float dy,int r,  Border *mborder, QWidget *parent):QWidget(parent){
+    //初始化对象
+    this->ballX = ballX;
+    this->ballY = ballY;
+    this->dx = dx;
+    this->dy = dy;
+    this->r = r;
+    this->mborder = mborder;
     g = 9.8; // 重力加速度
-    q = 0.9; // 碰撞系数
+    q = 0.8; // 碰撞系数
     t = 16; // 假设定时器更新时间为16ms ，使程序以60帧运行
 
+    timer = new QTimer(this);// 创建定时器对象
+    connect(timer,&QTimer::timeout,this,&Ball::updateBallPosition);// 连接定时器的timeout信号到更新小球位置的槽函数
 
-    timer = new QTimer(this);
-    connect(timer,&QTimer::timeout,this,&Ball::updateBallPosition);
 }
 
-
-
-void Ball::setSpeed(float x,float y){
-    dx = x;
-    dy = y;
-}
-void Ball::setRadius(int radius){
-    r = radius;
-}
-void Ball::setPosition(float x, float y){
-    ballX = x;
-    ballY = y;
+Border::Border(int rectX , int rectY , int rectWidth , int rectHeight){
+    //初始化
+    this->rectX = rectX;
+    this->rectY = rectY;
+    this->rectWidth = rectWidth;
+    this->rectHeight = rectHeight;
 }
 
-/*void Ball::setBorder(Border *b){
-    border = b;
-}*/
-
-void Border::setPosition(int x, int y, int width, int height){
-    rectX = x;
-    rectY = y;
-    rectWidth = width;
-    rectHeight = height;
-}
+bool Border::checkCollision(float x, float y,int r, Direction direction) {
+    switch (direction) {
+    case LEFT:
+    //检查小球是否碰撞到矩形框的左边界
+    //如果小球的左边缘（x - r）小于等于左边界的X坐标（rectX），则发生碰撞
+        return x - r <= rectX;
+    case RIGHT:
+    //检查小球是否碰撞到矩形框的右边界
+        return x + r >= rectX + rectWidth;
+    case TOP:
+    // 检查小球是否碰撞到矩形框的上方
+        return y - r <= rectY;
+    case BOTTOM:
+    //检查小球是否碰撞到矩形框的底部
+        return y + r >= rectY + rectHeight;
+    default:
+    //如果方向不匹配任何已知的方向，则不发生碰撞
+        return false;
+        }
+    }
 
 void Ball::startMoving(){
-    timer->start(t);
+    timer->start(t);//设置定时器，循环时间为t，可在后面设定
 }
 
 void Ball::paintEvent(QPaintEvent *event) {
     QWidget::paintEvent(event); // 调用基类的paintEvent函数
-
     QPainter painter(this);
-    draw(painter); // 调用draw函数进行绘制
+    mborder->draw(painter);//调用Border类的painter方法绘制矩形框
+    draw(painter);//调用Ball类的painter方法绘制小球
 }
 
 void Ball::draw(QPainter& painter) const {
-    // 绘制矩形框
-    painter.drawRect(border->getX(), border->getY(), border->getWidth(), border->getHeight());
-
-    // 绘制小球
+    //绘制小球
     painter.setBrush(Qt::red);//设置画刷为红色
     painter.drawEllipse(QPointF(ballX, ballY), r, r);
 }
 
-void Ball::updateBallPosition(){
-    // 使用border指针访问Border类的公共接口
-    int rectX = border->getX();
-    int rectY = border->getY();
-    int rectWidth = border->getWidth();
-    int rectHeight = border->getHeight();
-
-    // 小球的运动
-      ballX += dx;
-      ballY += dy;
-
-if (ballX <= rectX || ballX >= rectX + rectWidth - 2*r) {
-        dx = -dx; // 改变水平方向速度方向
-        dx = dx * q;
-      }
-
-if (ballY <= rectY || ballY >= rectY + rectHeight - 2*r) {
-        dy = -dy; // 改变垂直方向速度方向
-        dy = dy * q;
-
-        ballY = rectY + rectHeight - 2*r;//限制小球不会掉出矩形框
-      }
-
-      dy += (g / 1000 * t);//每次更新小球位置时，垂直速度加上重力加速度,单位换算成秒
-      update();//重绘窗口
+void Border::draw(QPainter& painter) const {
+    //绘制矩形框
+    painter.drawRect(rectX, rectY, rectWidth, rectHeight);
 }
 
-Ball::~Ball()//Ball类的析构函数
-{
-    delete ui;
-    /*timer->stop();
-    delete timer;*/
-    delete border;
+void Ball::updateBallPosition(){
+    //小球的运动
+    ballX += dx;
+    ballY += dy;
 
+    //检查小球是否碰撞到矩形框的左边或右边
+    if ((dx < 0 && mborder->checkCollision(ballX, ballY, r, LEFT)) ||
+        (dx > 0 && mborder->checkCollision(ballX, ballY, r, RIGHT))) {
+    //如果是，改变X方向上的速度
+        dx = -dx * q;// 改变水平方向速度方向和大小
+    }
+    // 检查小球是否碰撞到矩形框的上边或下边（地面）
+    if ((dy < 0 && mborder->checkCollision(ballX, ballY, r, TOP)) ||
+        (dy > 0 && mborder->checkCollision(ballX, ballY, r, BOTTOM))) {
+    // 如果是，改变Y方向上的速度
+        dy = -dy * q;//改变竖直方向速度方向和大小
+        ballY = mborder->rectY + mborder->rectHeight - 2*r;//防止小球掉出矩形框
+    }
+        dy += (g / 1000 * t);//每次更新小球位置时，垂直速度加上重力加速度,单位换算成秒
+    update();//重绘窗口
 }
--- main.cpp
+++ main.cpp
@@ -5,16 +5,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Border border;
-    Ball ball;
+    Border border(50,50,400,500);//设置参数的值，依次为矩形框左上角x坐标、矩形框左上角y坐标、矩形框宽度、矩形框高度
+    Ball ball(50,50,4,2,15,&border);//设置参数的值，分别为小球x坐标、小球y坐标、小球水平方向速度、小球竖直方向速度、小球半径和一个border指针
 
-    border.setPosition(50,50,400,500);
-    ball.setPosition(50,50);
-    ball.setSpeed(2,3);
-    ball.setRadius(5);
+    ball.resize(500,600);//设置窗口大小
+    ball.move(600,150);//移动窗口
+    ball.setFixedSize(ball.width(),ball.height());//锁定窗口大小
+    ball.setWindowTitle("运动小球");//窗口标题
     ball.startMoving();
-    Ball b;
-    b.show();
+    ball.show();
 
 
 
